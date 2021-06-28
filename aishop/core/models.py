b1d6827e953a214af91c5ea398794530170317d8
Praphan Oranphanlert <e23thr@gmail.com>
2021-06-28 10:13:25 +0000

    Initial django with docker dev container

---
+++ aishop/core/models.py
@@ -0,0 +1,39 @@
+from typing import Any
+from django.db import models
+
+class ModelWithMetadata(models.Model):
+    metadata = models.JSONField(
+        blank=True, null=True, default=dict, encoder="")
+
+    class Meta:
+        abstract = True
+
+    def get_value_from_private_metadata(self, key: str, default: Any = None) -> Any:
+        return self.private_metadata.get(key, default)
+
+    def store_value_in_private_metadata(self, items: dict):
+        if not self.private_metadata:
+            self.private_metadata = {}
+        self.private_metadata.update(items)
+
+    def clear_private_metadata(self):
+        self.private_metadata = {}
+
+    def delete_value_from_private_metadata(self, key: str):
+        if key in self.private_metadata:
+            del self.private_metadata[key]
+
+    def get_value_from_metadata(self, key: str, default: Any = None) -> Any:
+        return self.metadata.get(key, default)
+
+    def store_value_in_metadata(self, items: dict):
+        if not self.metadata:
+            self.metadata = {}
+        self.metadata.update(items)
+
+    def clear_metadata(self):
+        self.metadata = {}
+
+    def delete_value_from_metadata(self, key: str):
+        if key in self.metadata:
+            del self.metadata[key]
